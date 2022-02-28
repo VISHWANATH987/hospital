0e121cd4ad7bc242f54f090906069bdf3d56be47
VISHWANATH <71350788+VISHWANATH987@users.noreply.github.com>
2022-02-28 18:35:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
         val === 1 ? <WelcomeTitleDiv/>:null
       }
       {
-        val === 2 && val2 != 3 ?  <LoginDiv/>: null
+        val === 2 && val2 != 3 ?  <HomeDiv/>: null
       }
       {
         val2 === 3 ?  <HomeDiv/>: null
