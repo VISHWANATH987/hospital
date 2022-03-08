cc9fd0531e2c5d1e30bea518bbd7265cc4fa11fd
VISHWANATH <71350788+VISHWANATH987@users.noreply.github.com>
2022-03-08 15:45:17 +0530

    Update SixthSideBarElementDiv.js

--- src/Components/SixthSideBarElementDiv.js
+++ src/Components/SixthSideBarElementDiv.js
@@ -1,9 +1,16 @@
+
 import React from 'react';
+import DescriptionIcon from '@mui/icons-material/Description';
 
 function SixthSideBarElementDiv() {
-  return <div className='SixthSideBarElementDiv'>
-  <div className='SixthSBED1'>LOGOUT</div>  
-  </div>;
+  return (
+    <div className='PatientAddedSuccessDiv'>
+      <div className='PatientAddedSuccess'>
+      <div className='PatientAddedSuccessBottom'>GENERATE BILLS.</div>
+          <div className='PatientAddedSuccessTop'><DescriptionIcon className="checkBoxIcon"/></div>
+      </div>
+  </div>
+  );
 }
 
 export default SixthSideBarElementDiv;
